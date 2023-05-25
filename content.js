8a78bd569964965a1d045135b7737befae86c53a
Allan Pinheiro <allan.hal@gmail.com>
2023-05-25 00:20:07 -0300

    First Draft working

--- content.js
+++ content.js
@@ -1,32 +1,56 @@
-// setInterval(frame, 1000);
-// function frame() {
-//   if (window.filterGCLobby) {
-//     Array.from(document.getElementsByClassName('lobby-btn-big')).forEach((element) => {
-//       if (element.innerText !== 'ENTRAR') {
-//         var closest = element.closest('.sala-card-wrapper');
-//         if (closest && closest instanceof HTMLElement) {
-//           closest.style.display = 'none';
-//         }
-//       }
-//     });
-//   }
-// }
+setTimeout(async function () {
+  let table = document.getElementById('PrecosGPU');
+  if (!table) {
+    return;
+  }
+  var row = table.rows[0];
+  var performanceCell = row.insertCell(4);
+  var costEffectiveCell = row.insertCell(5);
+  performanceCell.innerHTML = '<b>Benchmark</b>';
+  costEffectiveCell.innerHTML = '<b>Benchmark/Price</b>';
 
-setTimeout(function () {
-  console.log('content.js');
+  const gpuNames = new Set();
+  for (let rowNumber = 2; rowNumber < table?.rows?.length; rowNumber++) {
+    const benchMark = 1000;
+    const text = table.rows[rowNumber].cells[0].innerHTML;
+    const gpuName = text.split('<b>')[1].split('</b>')[0].trim();
+    gpuNames.add(gpuName);
+  }
+  const uniqueGpus = Array.from(gpuNames);
+  console.log('uniqueGpus', uniqueGpus);
 
-  //gets table
-  var oTable = document.getElementById('PrecosGPU');
-  console.log('oTable', oTable);
-  oTable.onchange = () => {
-    console.log('changed');
-  };
+  const jsonData = await fetch('https://gpu-benchmark.onrender.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      keys: uniqueGpus,
+    }),
+  }).then((response) => response.json());
+  console.log('jsonData', jsonData);
+  const result = jsonData.map((gpu) => ({
+    [gpu.key]: gpu.result.value,
+  }));
+  console.log('result', result);
+  let resultJson = {};
+  jsonData.forEach((gpu) => {
+    resultJson = {
+      ...resultJson,
+      [gpu.key]: gpu.result.value,
+    };
+  });
+  console.log('resultJson', resultJson);
 
-  //gets rows of table
-  var rowLength = oTable.rows.length;
-  console.log('rowLength', rowLength);
+  for (let rowNumber = 2; rowNumber < table?.rows?.length; rowNumber++) {
+    const benchMark = 1000;
+    const text = table.rows[rowNumber].cells[0].innerHTML;
+    const gpuName = text.split('<b>')[1].split('</b>')[0].trim();
 
-  let sdfg = document.getElementById('PrecosGPU');
-  let data = [...sdfg.rows].map((t) => [...t.children].map((u) => u.innerText));
-  console.log(data);
-}, 2000);
+    const price = table.rows[rowNumber].cells[1].innerHTML.split('&nbsp;')[1].split('.').join('').split(',').join('.');
+    const perfCell = table.rows[rowNumber].insertCell(4);
+    const ceCell = table.rows[rowNumber].insertCell(5);
+    perfCell.innerHTML = 'Price: ' + price + ' / Bench: ' + resultJson[gpuName];
+    ceCell.innerHTML = Number((benchMark / price) * 100).toFixed(2);
+  }
+}, 1000);
--- index.js
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-import * as csv from "fast-csv";
-
-export const data = [];
-fs.createReadStream("gpu.csv")
-  .pipe(csv.parse({ headers: true }))
-  .on("error", (error) => console.error(error))
-  .on("data", (row) => data.push(row))
-  .on("end", () => console.log(data));
